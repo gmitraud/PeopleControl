4e8bdfa8dc48a0278e4e49bf8b82271a7f8dbd5d
gmitraud <gmitraud@hotmail.com>
2022-12-10 12:05:02 -0300

    fix(style): fixed application.css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.paths << Rails.root.join("videos")
+Rails.application.config.assets.precompile += %w( tablestyle.css )
+Rails.application.config.assets.precompile += %w( style.css )
