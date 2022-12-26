146171b0d701f55f602272e64d86a6a2760a451d
gmitraud <gmitraud@hotmail.com>
2022-12-26 13:15:33 -0300

    style - Added layout to municipes, new_municipes, show_municipes

--- app/models/municipe.rb
+++ app/models/municipe.rb
@@ -1,2 +1,5 @@
 class Municipe < ApplicationRecord
+    has_one_attached :photo
+    validates :cns, length: { is: 15 }, numericality: true
+    validates :cpf, length: { is: 11 }, numericality: true
 end
