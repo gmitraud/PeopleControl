962ce76cd6db3ddc1d46769d86ff898488616f24
gmitraud <gmitraud@hotmail.com>
2023-04-18 15:46:47 -0300

    fix: fixed wrong date selection on municipes creation

--- app/models/municipe.rb
+++ app/models/municipe.rb
@@ -4,6 +4,8 @@ class Municipe < ApplicationRecord
     accepts_nested_attributes_for :endereco, allow_destroy: true
 
     has_one_attached :photo
+
+    validates_inclusion_of :birthdate, in: Date.new(1900, 1, 1)..Date.today, message: "A Data de Nascimento deve ser anterior ou igual a data atual"
     
     validates :cns, length: { is: 15 }, numericality: true, presence: true
     validates :cpf, length: { is: 11 }, numericality: true, presence: true
